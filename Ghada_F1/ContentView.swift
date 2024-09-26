7eb3a82e2af19184f40260ff44a0100ed5bf9ba0
Ghada <ghada-alshabanat@hotmail.com>
2024-09-26 17:17:47 +0300

    Initial Commit

---
+++ Ghada_F1/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  Ghada_F1
+//
+//  Created by Ghada Alshabanat on 22/03/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Water tracker 💦")
+        
+            Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+                Text("Apple health")
+            
+            }
+            
+            Stepper(value: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(4)/*@END_MENU_TOKEN@*/, in: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Range@*/1...10/*@END_MENU_TOKEN@*/) {
+                Text("Cups to drink per day 0")
+            }
+            Button("continue") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            //Image(systemName: "globe")
+            //   .imageScale(.large)
+            // .foregroundStyle(.tint)
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ghada_F1/Ghada_F1App.swift
@@ -0,0 +1,17 @@
+//
+//  Ghada_F1App.swift
+//  Ghada_F1
+//
+//  Created by Ghada Alshabanat on 22/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Ghada_F1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
